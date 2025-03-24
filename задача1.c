b3868d49359bbeeb7e54a776230838fcd1676fe5
bbmymy <www.afanas.oleg@gmail.com>
2025-03-24 12:31:45 +0300

    Update задача1.c

--- задача1.c
+++ задача1.c
@@ -1,19 +1,32 @@
-def calculate_trip_cost(demand_level, distance_km, duration_min):
-    # Стоимость подачи
-    base_fare = demand_level * 10
-    # Стоимость за километр
-    cost_per_km = 15 * distance_km
-    # Стоимость за минуту
-    cost_per_min = 5 * duration_min
-    # Комиссия
-    commission = 29 + (demand_level - 1) * (69 - 29) / 99
-    
-    # Итоговая стоимость
-    total_cost = base_fare + cost_per_km + cost_per_min + commission
-    return total_cost
+#include <stdio.h>
 
-# Пример использования
-demand_level = 50
-distance_km = 10
-duration_min = 20
-print(calculate_trip_cost(demand_level, distance_km, duration_min))
+float calculate_trip_cost(int demand_level, float distance_km, float duration_min) {
+    // Стоимость подачи
+    float base_fare = demand_level * 10.0;
+    // Стоимость за километр
+    float cost_per_km = 15.0 * distance_km;
+    // Стоимость за минуту
+    float cost_per_min = 5.0 * duration_min;
+    // Комиссия
+    float commission = 29.0 + (demand_level - 1) * (69.0 - 29.0) / 99.0;
+
+    // Итоговая стоимость
+    return base_fare + cost_per_km + cost_per_min + commission;
+}
+
+int main() {
+    int demand_level;
+    float distance_km, duration_min;
+
+    printf("Введите уровень спроса (1-100): ");
+    scanf("%d", &demand_level);
+    printf("Введите длину маршрута (в км): ");
+    scanf("%f", &distance_km);
+    printf("Введите длительность маршрута (в мин): ");
+    scanf("%f", &duration_min);
+
+    float total_cost = calculate_trip_cost(demand_level, distance_km, duration_min);
+    printf("Стоимость поездки: %.2f рублей\n", total_cost);
+
+    return 0;
+}
